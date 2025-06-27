a8f36b2a2da07a433f9a1cbe098c25dd7d7f2a45
helix-77 <mdatikmouhtasimrahi@gmail.com>
2025-06-28 01:17:24 +0600

    git init

---
+++ app/welcome.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { View } from "react-native";
+import { useRouter } from "expo-router";
+
+import { Image } from "@/components/image";
+import { SafeAreaView } from "@/components/safe-area-view";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { H1, Muted } from "@/components/ui/typography";
+import { useColorScheme } from "@/lib/useColorScheme";
+import { ThemeToggle } from "@/components/ui/ThemeToggle";
+
+export default function WelcomeScreen() {
+	const router = useRouter();
+	const { colorScheme } = useColorScheme();
+	const appIcon =
+		colorScheme === "dark"
+			? require("@/assets/icon.png")
+			: require("@/assets/icon-dark.png");
+
+	return (
+		<SafeAreaView className="flex flex-1 bg-background p-4">
+			<ThemeToggle />
+			<View className="flex flex-1 items-center justify-center gap-y-4 web:m-4">
+				<Image source={appIcon} className="w-16 h-16 rounded-xl" />
+				<H1 className="text-center">WasteGuard</H1>
+				<Muted className="text-center">
+					An App that help users reduce food, grocery and cosmetics waste by
+					tracking expiry dates, suggesting better usage and recipes, and
+					managing pantry inventory.
+				</Muted>
+			</View>
+			<View className="flex flex-col gap-y-4 web:m-4">
+				<Button
+					size="default"
+					variant="default"
+					onPress={() => {
+						router.push("/sign-up");
+					}}
+				>
+					<Text>Sign Up</Text>
+				</Button>
+				<Button
+					size="default"
+					variant="secondary"
+					onPress={() => {
+						router.push("/sign-in");
+					}}
+				>
+					<Text>Sign In</Text>
+				</Button>
+			</View>
+		</SafeAreaView>
+	);
+}
